4745ea817f904575fbbfec655b24cbd979dc6ba6
Mike Stevens <mike.stevens@vubiz.com>
2016-09-06 17:11:04 -0400

    added notify alert window as well

--- js/adapt-contrib-textNotify.js
+++ js/adapt-contrib-textNotify.js
@@ -3,19 +3,18 @@ define(function(require) {
     var Text = require('components/adapt-contrib-text/js/adapt-contrib-text');
 
     var TextNotify = Text.extend({
-        /*
-        * NOTE: requires the following commit to work:
-        * https://github.com/taylortom/adapt_framework/commit/a7af2e3f8713979f3b8933ed6c443f254ec6eb27
-        */
+
         template: "text",
         postRender: function() {
             Text.prototype.postRender.apply(this);
             $('.component-body-inner a', this.$el).click(_.bind(this.onAnchorClicked, this));
+            $('.component-body-inner button', this.$el).click(_.bind(this.onAnchorClicked, this));
         },
 
         remove: function() {
             // danger! will remove all events on '.component-body-inner a'
             $('.component-body-inner a', this.$el).off( "click" );
+            $('.component-body-inner button', this.$el).off( "click" );
         },
 
         inview: function(event, visible, visiblePartX, visiblePartY) {
@@ -24,14 +23,30 @@ define(function(require) {
 
         onAnchorClicked: function(event) {
             var id = $(event.currentTarget).attr("id");
-            // only hijack anchors with id
-            if(id) {
-                event.preventDefault();
-                var popupData = this.model.get("_popupData")[id];
-                Adapt.trigger('notify:popup', {
-                    title: popupData.title,
-                    body: popupData.message
-                });
+            var notifyopt = this.model.get("_notifyopt");
+            
+            if (notifyopt === 'popup') {
+                // only hijack anchors with id
+                if(id) {
+                    event.preventDefault();
+                    var popupData = this.model.get("_popupData")[id];
+                    Adapt.trigger('notify:popup', {
+                        title: popupData.title,
+                        body: popupData.message
+                    });
+                    Adapt.trigger
+                }
+            } else if (notifyopt === 'alert') {
+                if(id) {
+                    event.preventDefault();
+                    var alertData = this.model.get("_alertData")[id];
+                    Adapt.trigger('notify:alert', {
+                        title: alertData.title,
+                        body: alertData.message,
+                        confirmText: alertData.confirmButton
+                    });
+                    Adapt.trigger
+                }
             }
         }
     });
